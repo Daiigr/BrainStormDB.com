284809915b519cf261a5e65f0dd42d1ad221def8
Daiigr <daiigrbr@gmail.com>
2023-02-05 15:03:32 +0300

    added Options.css, BrainStormTitle.css, SearchPage.js

---
+++ src/SearchPage.js
@@ -0,0 +1,26 @@
+import './BrainStormTitle.css';
+import './OptionsPill.css';
+
+function SearchPage() {
+  return (
+    <div>
+    <div className="brainstormdb-title">
+      <header className="brainstormdb-title-header">
+        <p>
+          BrainStormDB
+        </p>
+      </header>
+    </div>
+    <div className="options-pill">
+        <header className="options-pill-header">
+            <p>
+                Options
+            </p>
+        </header>
+    </div>
+    </div>
+    
+  );
+}
+
+export default SearchPage;
